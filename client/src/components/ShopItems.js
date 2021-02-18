852535963e0bd041985af977900a0bc9fa0e1c88
Harry Stead <harrystead9@gmail.com>
2021-02-18 11:38:38 +0000

    map through elements (exact presentation to be decided later

--- client/src/components/ShopItems.js
+++ client/src/components/ShopItems.js
@@ -1,32 +1,39 @@
-
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class ShopItems extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            displayItems: [],
-        }
-    }
-    componentDidMount() {
-        axios
-          .get("http://localhost:5000/items")
-          .then((response) => {
-            this.setState({ displayItems: response.data });
-            console.log(this.state.displayItems)
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    render() {
-        return (
-            <div>
-                <h2>shop items</h2>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayItems: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/items")
+      .then((response) => {
+        this.setState({ displayItems: response.data });
+        console.log(this.state.displayItems);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>shop items</h2>
+        <div>
+          {this.state.displayItems.map((item, index) => (
+            <p>
+                {item.name} {item.category}
+            </p>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ShopItems;
