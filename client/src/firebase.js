98315de6cccc11f5e87c8e908ca796dd9d4f257b
Harry Stead <harrystead9@gmail.com>
2021-03-04 13:59:41 +0000

    bbjb

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,11 +7,8 @@ const app = firebase.initializeApp({
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 });
 
 export const auth = app.auth();
 export default app;
-
-//dknskdKNKNK
